7343fd164483f6966fbdd9044fd81c2e58e937e1
huang.xinghui <huang.x.hui@gmail.com>
2016-10-18 10:37:28 +0800

    first initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# npm-init.js
+> Automate npm init with defaults
+
+## Usage
+
+`npm config set init-module yourpath/npm-init.js`
+
+## Example
+
+[npm-init.js](npm-init.js)
+
+See the documentation for the [init-package-json](https://github.com/npm/init-package-json) module for more informatio
---
+++ npm-init.js
@@ -0,0 +1,32 @@
+var USER = 'huang-x-h';
+
+module.exports = {
+  name: prompt('name', basename),
+  version: '0.1.0',
+  description: prompt('description'),
+  main: prompt('entry point', 'index.js'),
+  bin: prompt('create command name', function(val) {
+    if (val) {
+      var command = {};
+      command[val] = './cli.js';
+      return command;
+    }
+  }),
+  script: prompt('test command', function(s) {
+    return {test: s};
+  }),
+  repository: prompt('create github repo', 'yes', function(val) {
+    val = val.indexOf('y') !== -1 ? true : false;
+
+    if (val) {
+      return {
+        type: 'git',
+        url: 'https://github.com/' + USER + '/' + basename + '.git'
+      }
+    }
+  }),
+  homepage: "https://github.com/" + USER + "/" + basename,
+  keywords: prompt(function (val) { return val.split(/\s+/) }),
+  license: 'MIT',
+  author: 'huang.xinghui <huang.x.hui@gmail.com>'
+};
